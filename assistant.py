63b4daa0e42b934ecc80798f08db852085232051
AYUSHWAL03 <ayush.s.walekar03@gmail.com>
2023-10-20 20:46:46 +0530

    updated some features

--- assistant.py
+++ assistant.py
@@ -1,19 +1,23 @@
 import speech_recognition as sr
 import pyttsx3 as tts
 from helper import helper
+
 r = sr.Recognizer()
 m = sr.Microphone()
 wakeWords = "Ova"
-def SpeakTextLoud(text):
-    engine = tts.init()
-    engine.say(text)
-    engine.runAndWait()
-
 response = {
     "success" : True,
     "error" : None,
     "message" : None
 }
+userinput_timeout = 10 # to record the user input 
+def SpeakTextLoud(text):
+    engine = tts.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
+
 def recognizeSpeech(recognizer,microphone):
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
@@ -21,12 +25,12 @@ def recognizeSpeech(recognizer,microphone):
         SpeakTextLoud("Hello There, I am OVA ...")
         print("Hello There, I am OVA ...")
         try:
-            usersAudio = r.listen(source)
+            usersAudio = r.listen(source, timeout=3)
             response["message"] = r.recognize_google(usersAudio)
             if response["message"].lower() == wakeWords.lower():
                 SpeakTextLoud("How can I help you?")
                 print("How can I help you?")
-                userQuestion = r.listen(source)
+                userQuestion = r.listen(source,timeout=userinput_timeout)
                 response["message"] = r.recognize_google(userQuestion)
                 answers = helper(response["message"])
                 print(response["message"])
